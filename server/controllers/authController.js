610a3cac7f3c58fcaa1c71b3b047ad9402f32822
janith ishara <janith.ishara@intellimorph.ai>
2025-04-10 01:09:56 +0530

    set login and routes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,37 +1,85 @@
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
+import { generateVerificationCode } from "../utils/generateVerificationCode.js";
 
 export const register = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { fistName, lastName, email, password } = req.body;
 
-  const canSave = Boolean(name && email && password);
+  const canSave = Boolean(fistName && lastName && email && password);
   if (!canSave) {
-    return res.json({ success: false, message: "Missing Details" });
+    throw new Error("All fields are requires");
   }
 
   try {
     const existingUser = await userModel.findOne({ email });
     if (existingUser) {
-      return res.json({ success: false, message: "User already exists" });
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
+    const verifyOtp = generateVerificationCode();
 
-    const user = new userModel({ name, email, password: hashedPassword });
+    const user = new userModel({
+      fistName,
+      lastName,
+      email,
+      password: hashedPassword,
+      verifyOtp,
+      verifyOtpExpireAt: Date.now() + 24 * 60 * 60 * 1000,
+    });
     await user.save();
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "7d",
-    });
+    // don't send token in the register stage
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-      maxAge: 7 * 24 * 60 * 60 * 100,
+    // const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    //   expiresIn: "7d",
+    // });
+
+    // res.cookie("token", token, {
+    //   httpOnly: true,
+    //   secure: process.env.NODE_ENV === "production",
+    //   sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    //   maxAge: 7 * 24 * 60 * 60 * 100,
+    // });
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
     });
   } catch (error) {
     return res.json({ success: false, message: error.message });
   }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.json({
+      success: false,
+      message: "Email and password are required!",
+    });
+  }
+
+  try {
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    generateTokenAndSetCookie(res, user._id);
+
+    return res.json({ success: true });
+  } catch (error) {}
 };
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
+import authRoutes from "./routes/authRoute.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -19,5 +20,6 @@ app.use(
 );
 
 app.get("/", (req, res) => res.send("App working"));
+app.use("api/auth", authRoutes);
 
 app.listen(port, () => console.log(`Server started in PORT: ${port}`));
