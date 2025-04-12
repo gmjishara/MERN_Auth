1cc6a7b1c8c9841161c5b31701f9e66e46529815
janith ishara <janith.ishara@intellimorph.ai>
2025-04-13 00:17:38 +0530

    created react app and set redux store

---
+++ client/my-app/src/app/api/apiSlice.js
@@ -0,0 +1,49 @@
+// Or from '@reduxjs/toolkit/query' if not using the auto-generated hooks
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: "/",
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().auth.token;
+
+    // If we have a token set in state, let's assume that we should be passing it.
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+
+    return headers;
+  },
+});
+
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+  if (result.error && result.error.status === 401) {
+    // try to get a new token
+    const refreshResult = await baseQuery(
+      {
+        url: "/auth/refreshToken",
+        method: "POST",
+      },
+      api,
+      extraOptions
+    );
+    if (refreshResult.data) {
+      // store the new token
+      api.dispatch(tokenReceived(refreshResult.data));
+      // retry the initial query
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(loggedOut());
+    }
+  }
+  return result;
+};
+
+// initialize an empty api service that we'll inject endpoints into later as needed
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: baseQueryWithReauth,
+  endpoints: () => ({}),
+  tagTypes: ["Post"],
+});
